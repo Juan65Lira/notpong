79ae3275e407390f71f1608d414f1682bd5c5d5b
Kai Lira <boredhyppo@gmail.com>
2021-10-07 14:00:18 -0500

    Pallet movement

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import pygame as pg
+from enum import Enum, unique
+
+
+UP = -1
+NONE = 0
+DOWN = 1
 
 
 class Pallet:
     WIDTH = 30
     HEIGHT = 100
     COLOR = pg.Color('white')
+    SPEED = 500
 
-    def __init__(self, x: int, y: int):
+    def __init__(self, game, x: int, y: int):
+        self.game = game
         self.hitbox = pg.Rect(x, y, self.WIDTH, self.HEIGHT)
+
+    def move(self, direction: int):
+        self.hitbox.move_ip((0, self.SPEED/60 * direction))
+        self.hitbox.clamp_ip(self.game.screen_rect)
 
     def draw(self, surface):
         pg.draw.rect(surface, self.COLOR, self.hitbox)
@@ -21,11 +33,12 @@ class NotPong:
         pg.init()
 
         self.screen = pg.display.set_mode((self.width, self.height))
+        self.screen_rect = self.screen.get_rect()
 
         gap = self.width // 30
         y = self.height//2 - Pallet.HEIGHT//2
-        self.left_plt = Pallet(gap, y)
-        self.right_plt = Pallet(self.width - gap - Pallet.WIDTH, y)
+        self.left_plt = Pallet(self, gap, y)
+        self.right_plt = Pallet(self, self.width - gap - Pallet.WIDTH, y)
 
     def mainloop(self):
         clock = pg.time.Clock()
@@ -38,6 +51,18 @@ class NotPong:
                 elif ev.type == pg.KEYDOWN:
                     if ev.key == pg.K_ESCAPE:
                         running = False
+
+            keys = pg.key.get_pressed()
+
+            if keys[pg.K_UP]:
+                self.right_plt.move(UP)
+            if keys[pg.K_DOWN]:
+                self.right_plt.move(DOWN)
+
+            if keys[pg.K_w]:
+                self.left_plt.move(UP)
+            if keys[pg.K_s]:
+                self.left_plt.move(DOWN)
 
             self.screen.fill(pg.Color('black'))
             self.left_plt.draw(self.screen)
